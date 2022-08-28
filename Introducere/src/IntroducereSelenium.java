9b4ea050f1908c87a7928fa6e72407a26ceca316
mvi004 <meleru.iulian04@yahoo.com>
2022-08-28 11:30:36 +0200

    selenium exercises

---
+++ Introducere/src/IntroducereSelenium.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IntroducereSelenium {
+
+	public static void main(String[] args) {
+	//Invoking Browser
+	//chromedriver.exe -> Chrome browser
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\meler\\Downloads\\chromedriver_win32//chromedriver.exe");
+	ChromeDriver driver = new ChromeDriver();
+	
+	driver.get("https://myelectrica.ro/index.php?pagina=login");
+	System.out.println(driver.getCurrentUrl());
+	driver.findElement(By.name("myelectrica_utilizator")).sendKeys("mail@yahoo.com");
+	driver.findElement(By.xpath("//*[@id=\"myelectrica_pass\"]")) .sendKeys("1234");
+	driver.findElement(By.id("myelectrica_login_btn")) .click();
+	
+	driver.close();
+	
+	}
+
+}
